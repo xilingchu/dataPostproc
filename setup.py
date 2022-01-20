9855acc2598b7d8ded86713db897f68a46e10fa1
Lingchu Xi <xilingchu@163.com>
2022-01-20 09:07:13 +0800

    Using a setup to install the package.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+        name='dataPostproc',
+        version='0.1.0',
+        description='Postprocessing code for DNS data.',
+        requires=['h5py'],
+        url='http://github.com/xilingchu/dataPostproc',
+        author='Xi Lingchu',
+        author_email='xilingchu@163.com',
+        license='MIT',
+        packages=['dataPostproc', 'dataPostproc.output', 'dataPostproc.utils'],
+        scripts=['scripts/dataPostproc']
+        )
