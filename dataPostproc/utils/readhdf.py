6a89a1d2059f723939206ea36880d7e831858e30
Lingchu Xi <xilingchu@163.com>
2024-12-27 18:55:46 +0800

    Fixup: optimize the funtion readhdf.

--- dataPostproc/utils/readhdf.py
+++ dataPostproc/utils/readhdf.py
@@ -1,6 +1,8 @@
+# pyright: reportUnusedVariable=false, reportInvalidTypeForm=false, reportAttributeAccessIssue=false, reportIndexIssue=false
+# The h5py.File will return Groups/Dataset/Datatype according to the file. Just ignore the diagnostics.
 import h5py
-import numpy as np
 from .decorators import _exist_file
+
 
 @_exist_file('_fn')
 def _readHDF(_fn=None, _var=None, _blockz=None, _blocky=None, _blockx=None):
@@ -11,25 +13,26 @@ def _readHDF(_fn=None, _var=None, _blockz=None, _blocky=None, _blockx=None):
     blockx, blocky and blockz are selected choices.
 
     Details of blockx(yz) are blockx[start, stride, count, block]
-    
+
     start  ---- The start point of the block
     stride ---- The length of two blocks
     count  ---- The count of the blocks
     block  ---- The size of one block
     '''
     with h5py.File(_fn, 'r') as f:
-        sh_in = f[_var].shape
+        _var_in = f[_var]
+        sh_in = _var_in.shape
         dims = len(sh_in)
         if dims == 1:
-            if _blockz == None:
-                _blockz=[0, 1, sh_in[0], 1]
+            if _blockz is None:
+                _blockz = [0, 1, sh_in[0], 1]
             if sh_in[0] == 1:
                 _var_out = f[_var][0]
             else:
                 _var_out = f[_var][h5py.MultiBlockSlice(_blockz[0], _blockz[1], _blockz[2], _blockz[3])][:]
         elif dims == 2:
-            if _blockz == None:
-                _blockz=[0, 1, sh_in[0], 1]
+            if _blockz is None:
+                _blockz = [0, 1, sh_in[0], 1]
             if _blocky == None:
                 _blocky=[0, 1, sh_in[1], 1]
             _var_out = f[_var][h5py.MultiBlockSlice(_blockz[0], _blockz[1], _blockz[2], _blockz[3]),
@@ -44,5 +47,6 @@ def _readHDF(_fn=None, _var=None, _blockz=None, _blocky=None, _blockx=None):
             _var_out = f[_var][h5py.MultiBlockSlice(_blockz[0], _blockz[1], _blockz[2], _blockz[3]),
                              h5py.MultiBlockSlice(_blocky[0], _blocky[1], _blocky[2], _blocky[3]),
                              h5py.MultiBlockSlice(_blockx[0], _blockx[1], _blockx[2], _blockx[3])][:,:,:]
-        f.close()
+        else:
+            raise Exception('The maximum dimension is 3.')
     return _var_out
