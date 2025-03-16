d2250397e9714b2f3862a6d4fa8c97c260c33fa6
Lingchu Xi <xilingchu@163.com>
2025-03-16 18:24:25 +0800

    Optimize the code of abcH5.

--- dataPostproc/abcH5.py
+++ dataPostproc/abcH5.py
@@ -3,136 +3,150 @@
 from abc import ABC, abstractmethod
 from pathlib import Path
 from dataPostproc.utils.toolbox import _funlib
+from dataPostproc.utils.decorators import _exist_file
 import h5py
 
 
+def _del_repeated(_list: list[str]) -> list[str]:
+    """Remove duplicate items from a list while preserving order."""
+    if not isinstance(_list, list):
+        raise TypeError("Input must be a list.")
+    return list(dict.fromkeys(_list))
+
+@_exist_file('file_path')
+def getVarlist(file_path: Path = Path('')) -> list[str]:
+    """Retrieve a list of variable names from an HDF5 file.
+
+    Args:
+        file_path: Path to the HDF5 file. Defaults to an empty Path.
+
+    Returns:
+        A list of variable names (keys) in the HDF5 file.
+    """
+    with h5py.File(file_path, 'r') as f:
+        return list(f.keys())
+
+class requiredError(Exception):
+    pass
+
+class directionError(Exception):
+    pass
+
 class abcH5(ABC):
     def __init__(self, **kwargs):
-
-        def getVarlist(_fn=Path('')):
-            with h5py.File(_fn, 'r') as f:
-                _varlist = list(f.keys())
-                return _varlist
-
-        def _del_repeated(_list):
-            for _item in _list:
-                if _list.count(_item) > 1:
-                    _list.pop(_item)
-            return _list
-
         self._funlib  = _funlib()
+        self._validate_required_args(kwargs)
+        self._check_direction(kwargs)
+        self._check_variables(kwargs)
+        self._check_periodic(kwargs)
 
-        if '_fn' not in kwargs.keys() or '_list' not in kwargs.keys():
-            raise Exception('You must have _fn or _dire in this class. Please recheck the code!')
+        # Validate required Error
+        def _validate_required_args(self, kwargs):
+            if '_fn' not in kwargs.keys() or '_list' not in kwargs.keys():
+                raise requiredError('You must provide both _fn and _list in this class. Please recheck the code!')
 
-        # Direction
-        if '_dire' not in kwargs.keys():
-            self._dire = 'x'
-        else:
-            self._dire = kwargs['_dire']
-            kwargs.pop('_dire')
-
-        # filename
-        self._fn   = Path(kwargs['_fn']).expanduser().resolve()
-        if not self._fn.exists():
-            raise FileExistsError('ERROR: Target file doesn\'t exist. Please check your input file!')
-        if not h5py.is_hdf5(self._fn):
-            raise FileExistsError('ERROR: Target file is not valid HDF5 file. Please check your input file!')
-        kwargs.pop('_fn')
-
-        # Block in three directions
-        if '_blockx' in kwargs.keys():
-            _blockx = kwargs['_blockx']
-            kwargs.pop('_blockx')
-        else:
-            _blockx = None
-        if '_blocky' in kwargs.keys():
-            _blocky = kwargs['_blocky']
-            kwargs.pop('_blocky')
-        else:
-            _blocky = None
-        if '_blockz' in kwargs.keys():
-            _blockz = kwargs['_blockz']
-            kwargs.pop('_blockz')
-        else:
-            _blockz = None
-        self._blockx = _blockx
-        self._blocky = _blocky
-        self._blockz = _blockz
-
-        # Define the varlist
-        self._varlist = getVarlist(_fn = self._fn)
-
-        # Check the list and define the list
-        _list = kwargs['_list'] 
-        _list = _del_repeated(_list)
-        kwargs.pop('_list')
-        for _item in _list:
-            if _item not in self._varlist and _item not in self._funlib._funlist:
-                print('Warning: Please check the variable {}, which is not in the list!'.format(_item))
-                print(self._varlist)
-                _list.remove(_item)
-        self._list = _list
-        
-        # Check if the list is periodic
-        self._file = h5py.File(self._fn, 'r')
-        # Judge the shape and the array.
-        # X-direction
-        try:
-            self._sx   = self._file['x'].shape[0]
-            self._varx = 'x'
-        except:
-            try:
-                self._sx   = self._file['xc'].shape[0]
-                self._varx = 'xc'
-                if self._dire == 'x':
-                    self._dire = 'xc'
-            except:
-                raise Exception('The HDF5 file don\'t have array contains the data of the x direction.')
-
-        # Y-direction
-        try:
-            self._sy   = self._file['y'].shape[0]
-            self._vary = 'y'
-        except:
-            try:
-                self._sy = self._file['yc'].shape[0]
-                self._vary = 'yc'
-                if self._dire == 'y':
-                    self._dire = 'yc'
-            except:
-                raise Exception('The HDF5 file don\'t have array contains the data of the y direction.')
-
-        # Z-direction
-        try:
-            self._sz   = self._file['z'].shape[0]
-            self._varz = 'z'
-        except:
-            try:
-                self._sz = self._file['zc'].shape[0]
-                self._varz = 'zc'
-                if self._dire == 'z':
-                    self._dire = 'zc'
-            except:
-                raise Exception('The HDF5 file don\'t have array contains the data of the z direction.')
-
-        self._per  = {}
-        for _item in self._list:
-            if _item not in self._funlib._funlist:
-                _shape = self._file[_item].shape
-                self._per[_item] = [int(_shape[1] == self._sy + 2), int(_shape[2] == self._sx + 2)]
+        # Initialize the direction
+        def _check_direction(self, kwargs):
+            # Direction
+            if '_dire' not in kwargs.keys():
+                self._dire = 'x'
             else:
-                _var   = self._funlib._perdict[_item]
-                _shape = self._file[_var].shape
-                self._per[_item] = [int(_shape[1] == self._sy + 2), int(_shape[2] == self._sx + 2)]
+                self._dire = kwargs['_dire']
+                kwargs.pop('_dire')
 
+        # Define the variables
+        def _check_variables(self, kwargs):
+            # filename
+            self._fn   = Path(kwargs['_fn']).expanduser().resolve()
+            kwargs.pop('_fn')
+            # Define the varlist
+            self._varlist = getVarlist(self._fn)
+
+            # Check the list and define the list
+            _list = kwargs['_list'] 
+            _list = _del_repeated(_list)
+            kwargs.pop('_list')
+            to_remove = []
+            for _item in _list:
+                if _item not in self._varlist and _item not in self._funlib._funlist:
+                    print(f'Warning: Please check the variable {_item}, which is not in the list!')
+                    print(self._varlist)
+                    to_remove.append(_item)
+            self._list = [item for item in _list if item not in to_remove]
+
+        def _check_periodic(self):
+            # Block in three directions
+            if '_blockx' in kwargs.keys():
+                _blockx = kwargs['_blockx']
+                kwargs.pop('_blockx')
+            else:
+                _blockx = None
+            if '_blocky' in kwargs.keys():
+                _blocky = kwargs['_blocky']
+                kwargs.pop('_blocky')
+            else:
+                _blocky = None
+            if '_blockz' in kwargs.keys():
+                _blockz = kwargs['_blockz']
+                kwargs.pop('_blockz')
+            else:
+                _blockz = None
+            self._blockx = _blockx
+            self._blocky = _blocky
+            self._blockz = _blockz
+
+            # Check if the list is periodic
+            self._file = h5py.File(self._fn, 'r')
+            # Judge the shape and the array.
+            # X-direction
+            try:
+                self._sx   = self._file['x'].shape[0]
+                self._varx = 'x'
+            except:
+                try:
+                    self._sx   = self._file['xc'].shape[0]
+                    self._varx = 'xc'
+                    if self._dire == 'x':
+                        self._dire = 'xc'
+                except:
+                    raise directionError('The HDF5 file does not contain data for the x direction.')
+
+            # Y-direction
+            try:
+                self._sy   = self._file['y'].shape[0]
+                self._vary = 'y'
+            except:
+                try:
+                    self._sy = self._file['yc'].shape[0]
+                    self._vary = 'yc'
+                    if self._dire == 'y':
+                        self._dire = 'yc'
+                except:
+                    raise directionError('The HDF5 file does not contain data for the y direction.')
+
+            # Z-direction
+            try:
+                self._sz   = self._file['z'].shape[0]
+                self._varz = 'z'
+            except:
+                try:
+                    self._sz = self._file['zc'].shape[0]
+                    self._varz = 'zc'
+                    if self._dire == 'z':
+                        self._dire = 'zc'
+                except:
+                    raise directionError('The HDF5 file does not contain data for the z direction.')
+
+            self._per  = {}
+            for _item in self._list:
+                if _item not in self._funlib._funlist:
+                    _shape = self._file[_item].shape
+                    self._per[_item] = [int(_shape[1] == self._sy + 2), int(_shape[2] == self._sx + 2)]
+                else:
+                    _var   = self._funlib._perdict[_item]
+                    _shape = self._file[_var].shape
+                    self._per[_item] = [int(_shape[1] == self._sy + 2), int(_shape[2] == self._sx + 2)]
+            
         @abstractmethod
         def _output(self, _fn:str) -> None:
-            '''
-            _output: Implement by the other class to output file.
-            -- Input: 
-                - filename: The output name.
-            -- Returns:
-                Output the file.
-            '''
-            raise Exception("This output is incorrectly implemented.")
+            pass
--- dataPostproc/utils/decorators.py
+++ dataPostproc/utils/decorators.py
@@ -1,23 +1,38 @@
 from functools import wraps
 from pathlib import Path
 import h5py
-import sys
+import logging
+from typing import Callable, Dict, Any
 
-#------ Decorator ------#
-def _exist_file(_var_fn):
-    def decorator(func):
+# Configure logging
+logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger(__name__)
+
+# Custom Exceptions
+class InvalidFilenameError(Exception):
+    pass
+
+class InvalidHDF5FileError(Exception):
+    pass
+
+# Decorator
+def _exist_file(_var_fn: str) -> Callable:
+    def decorator(func: Callable) -> Callable:
         @wraps(func)
-        def wrapper(**kwargs):
-            if _var_fn not in kwargs.keys():
-                print('Warning: The variable of filename is incorrect, maybe you should check it!')
-            else:
-                filename = Path(kwargs[_var_fn]).absolute().resolve()
-                if not filename.exists():
-                    print('ERROR: Target file doesn\'t exist. Please check your input file!')
-                    raise FileExistsError
-                if not h5py.is_hdf5(filename):
-                    print('ERROR: Target file is not valid HDF5 file. Please check your input file!')
-                    raise FileExistsError
-                return func(**kwargs)
+        def wrapper(**kwargs: Dict[str, Any]) -> Any:
+            if _var_fn not in kwargs:
+                logger.warning("The variable of filename is incorrect. Please check it!")
+                raise InvalidFilenameError(f"Missing required argument: {_var_fn}")
+
+            filename = Path(str(kwargs[_var_fn])).resolve()
+            if not filename.exists():
+                logger.error("Target file doesn't exist. Please check your input file!")
+                raise FileNotFoundError(f"File not found: {filename}")
+
+            if not h5py.is_hdf5(filename):
+                logger.error("Target file is not a valid HDF5 file. Please check your input file!")
+                raise InvalidHDF5FileError(f"Invalid HDF5 file: {filename}")
+
+            return func(**kwargs)
         return wrapper
     return decorator
